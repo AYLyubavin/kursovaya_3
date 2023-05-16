11a05bf182779c84e7ee082d8a822870b6790bc1
lyubavin <aylyubavin@yandex.ru>
2023-05-16 18:05:30 +0300

    add files final version

---
+++ src/funcs.py
@@ -0,0 +1,60 @@
+import json
+
+
+def load_data(path):
+    """Загружает данные о трансакциях из словаря .json"""
+    with open(path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
+
+
+def get_filtered_data(data):
+    """ Фильтрует данные по параметру "EXECUTED" и создает новый список транзакций"""
+    filtered_data = []
+    for i in data:
+        if i.get("state") == "EXECUTED":
+            filtered_data.append(i)
+    return filtered_data
+
+
+def dict_modification(data):
+    """Перепаковывает словари в списке, оставляя только требуемые параметры и добавляя параметры,
+    которые в словаре отсутствуют, сортирует список по датеgit"""
+    new_list = []
+    for item in data:
+        new_dict = {}
+        new_dict["date"] = item["date"][:10]
+        new_dict["description"] = item["description"]
+        try:
+            new_dict["from"] = item["from"]
+        except KeyError:
+            new_dict["from"] = "Not found"
+        new_dict["to"] = item["to"]
+        new_dict["amount"] = item["operationAmount"]["amount"]
+        new_dict["currency"] = item["operationAmount"]["currency"]["name"]
+        new_dict["currency"] = item["operationAmount"]["currency"]["name"]
+        new_list.append(new_dict)
+    return sorted(new_list, key=lambda x: x["date"], reverse=True)
+
+def date_formatted(new_list):
+    """Переписывает дату в требуемом формате"""
+    for i in new_list:
+        year, month, day = i["date"].split("-")
+        i["date"] = f"{day}.{month}.{year}"
+    return new_list
+
+def masked_from(from_param):
+    """Закрывает часть информации в выводе в соответствие с требованиями"""
+    if len(from_param) == 25:
+        return f"Счет **{from_param[-4:]}"
+    elif len(from_param) == 9:
+        return from_param
+    else:
+        return f"{from_param[:-12]} {from_param[-11:-10]}** **** {from_param[-4:]} ->"
+
+def masked_to(to_param):
+    """Закрывает часть информации в выводе в соответствие с требованиями"""
+    if len(to_param) == 25:
+        return f"Счет **{to_param[-4:]}"
+    else:
+        return f"{to_param[:-12]} {to_param[-11:-10]}** **** {to_param[-4:]}"
---
+++ tests/test_funcs.py
@@ -0,0 +1,46 @@
+import pytest
+from src.funcs import load_data, get_filtered_data, dict_modification, date_formatted, masked_from, masked_to
+
+@pytest.fixture
+def test_data():
+    return [{'id': 41428829, 'state': 'FAILED', 'date': '2019-07-03T18:35:29.512364',
+                         'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}},
+                         'description': 'Перевод организации', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'},
+            {'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
+                         'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+                         'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572',
+                         'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}},
+                         'description': 'Перевод организации', 'to': 'Счет 11776614605963066702'},
+            {'id': 587085106, 'state': 'EXECUTED', 'date': '2018-03-23T10:45:06.972075',
+                         'operationAmount': {'amount': '48223.05', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+                         'description': 'Открытие вклада', 'to': 'Счет 41421565395219882431'},]
+
+
+def test_load_data(test_data):
+    data = load_data("./tests/operations.json")
+    assert isinstance(data, list)
+
+def test_get_filtered_data(test_data):
+    assert get_filtered_data(test_data[:2]) == [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
+                         'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+                         'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'}]
+
+
+def test_dict_modification(test_data):
+    assert dict_modification(test_data[1:3]) == [{'date': '2019-08-26', 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589', 'amount': '31957.58', 'currency': 'руб.'},
+ {'date': '2018-06-30', 'description': 'Перевод организации', 'from': 'Not found', 'to': 'Счет 11776614605963066702', 'amount': '9824.07', 'currency': 'USD'}]
+
+
+def test_date_formatted(test_data):
+    n = [{'date': '2019-12-08', 'description': 'Открытие вклада', 'from': 'Not found', 'to': 'Счет 90424923579946435907', 'amount': '41096.24', 'currency': 'USD'}]
+    assert date_formatted(n) == [{'date': '08.12.2019', 'description': 'Открытие вклада', 'from': 'Not found', 'to': 'Счет 90424923579946435907', 'amount': '41096.24', 'currency': 'USD'}]
+
+def test_masked_from(test_data):
+    assert masked_from('Visa Classic 2842878893689012') == "Visa Classic 2842 7** **** 9012 ->"
+    assert masked_from('Счет 27248529432547658655') == "Счет **8655"
+    assert masked_from("Not found") == "Not found"
+
+def test_masked_to(test_data):
+    assert masked_to('Visa Classic 2842878893689012') == "Visa Classic 2842 7** **** 9012"
+    assert masked_to('Счет 27248529432547658655') == "Счет **8655"
